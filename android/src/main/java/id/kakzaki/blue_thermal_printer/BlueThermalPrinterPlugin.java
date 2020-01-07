0987e14af96250dba3f2d7fd11d1ea562c3d2cde
Sandhy Sanjaya <sandhysanjaya1202@gmail.com>
2020-01-07 15:18:25 +0700

    ubah

--- android/src/main/java/id/kakzaki/blue_thermal_printer/BlueThermalPrinterPlugin.java
+++ android/src/main/java/id/kakzaki/blue_thermal_printer/BlueThermalPrinterPlugin.java
@@ -538,7 +538,7 @@ public class BlueThermalPrinterPlugin implements MethodCallHandler, RequestPermi
     }
     try {
       THREAD.write(cc);
-      String line = String.format("%-10s %15s %15s %n", "1", "Nasi Goreng", "15.000.0");
+      String line = String.format("%-10s %1s %15s %n", msg1, msg2, msg3);
       THREAD.write(line.getBytes());
       result.success(true);
     } catch (Exception ex) {
