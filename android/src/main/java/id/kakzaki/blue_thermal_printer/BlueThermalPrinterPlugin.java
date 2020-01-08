517395cef566cd1a3316cc4dc02501c88319a948
Sandhy Sanjaya <sandhysanjaya1202@gmail.com>
2020-01-08 07:08:19 +0700

    ubah

--- android/src/main/java/id/kakzaki/blue_thermal_printer/BlueThermalPrinterPlugin.java
+++ android/src/main/java/id/kakzaki/blue_thermal_printer/BlueThermalPrinterPlugin.java
@@ -585,7 +585,7 @@ public class BlueThermalPrinterPlugin implements MethodCallHandler, RequestPermi
       if(msg3.equals(empty)){
         adjusment = 15;
       }else{
-        adjusment =  marginBetweenMiddleAndRight - 20;
+        adjusment =  marginBetweenMiddleAndRight - 14;
       }
       Log.v(TAG, "msg3 : " + msg3);
       Log.v(TAG, "marginBetweenMiddleAndRight : " + marginBetweenMiddleAndRight);
